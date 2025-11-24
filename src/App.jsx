f4039c6f834593aaf7aef4faaf6a6262230fc485
nwmnwd <nyomanwidianaa96@gmail.com>
2025-11-24 22:42:25 +0800

    add  vercel analytic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Header from "./components/Header.jsx";
 import SearchBox from "./components/SearchBox.jsx";
 import SchedulePage from "./components/SchedulePage.jsx";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 
 function App() {
-  return <SchedulePage />;
+  return (
+    <>
+      <SchedulePage />
+
+      <Analytics />
+    </>
+  );
 }
 export default App;
