e6599c2f9e30b36f9d8d7fb83e3c61057e14a0c0
nwmnwd <nyomanwidianaa96@gmail.com>
2025-11-24 22:55:14 +0800

    fix analytic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Header from "./components/Header.jsx";
-import SearchBox from "./components/SearchBox.jsx";
 import SchedulePage from "./components/SchedulePage.jsx";
-
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <SchedulePage />
+      <Analytics />
     </>
   );
 }
